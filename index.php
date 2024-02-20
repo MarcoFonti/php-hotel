eab04f5c7f74ae6ea360a07314063819acdcecce
MarcoFonti <theemporiumg@gmail.com>
2024-02-20 15:52:12 +0100

    add scaffolding

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+// RECUPERO DATI
+require './data/info.php'
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Hotel</title>
+</head>
+<body>
+    <h1>HOTEL</h1>
+</body>
+</html>
